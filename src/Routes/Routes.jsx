557fef6136786f04b384922f240118f3dc807ef6
foysal90 <foysal1416@hotmail.com>
2024-01-29 22:26:11 -0500

    displayed foodn item in different tabs implemented

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,25 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
-import OrderFood from "../pages/OrderFood/OrderFood";
+import OrderFood from "../pages/Order/OrderFood/OrderFood";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
-        {
-            path: '/',
-            element: <Home/>
-        },
-        {
-            path: '/menu',
-            element: <Menu/>
-        },
-        {
-            path: '/order',
-            element: <OrderFood/>
-        }
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/order",
+        element: <OrderFood />,
+      },
     ],
   },
 ]);
--- src/pages/Order/OrderFood/OrderFood.jsx
+++ src/pages/Order/OrderFood/OrderFood.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
-import orderCoverImg from "../../assets/shop/banner2.jpg";
-import Cover from "../Shared/Cover/Cover";
+import orderCoverImg from "../../../assets/shop/banner2.jpg"
+import Cover from "../../Shared/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import useMenu from "../../hooks/useMenu";
+import useMenu from "../../../hooks/useMenu";
 import { Helmet } from "react-helmet-async";
-import FoodCard from "../../components/FoodCard/FoodCard";
+import FoodCard from "../../../components/FoodCard/FoodCard";
+import OrderTab from "../OrderTab/OrderTab";
 const OrderFood = () => {
   const [foodTab, setFoodTab] = useState(0);
   const [menu] = useMenu();
 
   //const salad = menu.map(item => item.category === 'salads')
-  const offered = menu.filter((item) => item.category === "offered");
+  //const offered = menu.filter((item) => item.category === "offered");
   const dessert = menu.filter((item) => item.category === "dessert");
   const pizza = menu.filter((item) => item.category === "pizza");
   const salad = menu.filter((item) => item.category === "salad");
   const soup = menu.filter((item) => item.category === "soup");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
-        <Helmet> <title>TOH | order </title></Helmet>
+      <Helmet>
+        {" "}
+        <title>TOH | order </title>
+      </Helmet>
       <Cover img={orderCoverImg} title={"order food"} />
       <Tabs defaultIndex={foodTab} onSelect={(index) => setFoodTab(index)}>
         <TabList>
@@ -32,18 +37,20 @@ const OrderFood = () => {
         </TabList>
 
         <TabPanel>
-           <div className="grid md: grid-cols-3 gap-5 m-10">
-           {
-                salad.map(item => <FoodCard key={item._id} item={item}/>)
-            }
-           </div>
+          <OrderTab items={salad}/>
+        </TabPanel>
+        <TabPanel> 
+          <OrderTab items={pizza} />
+          </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup}/>
         </TabPanel>
         <TabPanel>
-
+          <OrderTab items={dessert}/>
         </TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}/>
+        </TabPanel>
       </Tabs>
     </div>
   );
